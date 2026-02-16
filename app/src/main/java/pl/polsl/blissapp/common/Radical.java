1fc5a20afa02efa8c93623d49485c1dbd0f3167d
kando-123 <kamilgwozdz001@gmail.com>
2026-02-16 14:46:35 +0100

    Zdefiniowano konstruktor typu wyliczeniowego Radical.

--- app/src/main/java/pl/polsl/blissapp/common/Radical.java
+++ app/src/main/java/pl/polsl/blissapp/common/Radical.java
@@ -1,6 +1,32 @@
 package pl.polsl.blissapp.common;
 
+import java.util.Collections;
+import java.util.List;
+
 public enum Radical
 {
-    
+    HORIZONTAL_WAVE("wav"),
+    VERTICAL_WAVE("wav3"),
+
+    HEART("hrt"),
+
+    EAR("ear"),
+
+    BIG_CIRCLE("crlB"),
+    SMALL_CIRCLE("crlS"),
+
+    ETC("etc")
+    ;
+
+    Radical(String code)
+    {
+        this.code = code;
+    }
+
+    private final String code;
+
+    public String code()
+    {
+        return code;
+    }
 }
