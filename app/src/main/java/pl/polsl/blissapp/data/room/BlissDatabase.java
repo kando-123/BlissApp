3e871ca9fb60b3690c80cdae58f99723a9f65161
kando-123 <kamilgwozdz001@gmail.com>
2026-03-28 15:35:38 +0100

    Z tabeli AlchemyProgress usunięto symbole liter, cyfr i interpunkcji oraz symbole związane z tematyką seksualną. (Same słowa: płeć, płeć męska, płeć żeńska pozostały, ale seks analny, seks oralny i inne takie zostały usunięte.)

--- app/src/main/java/pl/polsl/blissapp/data/room/BlissDatabase.java
+++ app/src/main/java/pl/polsl/blissapp/data/room/BlissDatabase.java
@@ -34,19 +34,6 @@ public abstract class BlissDatabase extends RoomDatabase
 {
     private static volatile BlissDatabase INSTANCE;
 
-//    static final Migration MIGRATION_4_5 = new Migration(4, 5)
-//    {
-//        @Override
-//        public void migrate(@NonNull SupportSQLiteDatabase database)
-//        {
-//            database.execSQL("CREATE TABLE IF NOT EXISTS `AlchemyProgress` (" +
-//                    "`symbol_index` INTEGER NOT NULL, " +
-//                    "`is_discovered` INTEGER NOT NULL DEFAULT 0, " +
-//                    "PRIMARY KEY(`symbol_index`), " +
-//                    "FOREIGN KEY(`symbol_index`) REFERENCES `Symbol`(`symbol_index`) ON UPDATE NO ACTION ON DELETE CASCADE )");
-//        }
-//    };
-
     public static BlissDatabase getDatabase(final Context context)
     {
         if (INSTANCE == null)
@@ -59,7 +46,6 @@ public abstract class BlissDatabase extends RoomDatabase
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             BlissDatabase.class, "Symbol.db")
                             .createFromAsset("databases/Symbol.db")
-//                            .addMigrations(MIGRATION_4_5)
                             .fallbackToDestructiveMigration(true)
                             .build();
                     Log.d("BlissDatabase", "Database created");
